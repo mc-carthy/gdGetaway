6ba17efe4abad301f448a838aa4124c45f2c141f
Mick McCarthy <mick@mccarthy.tech>
2020-04-25 18:16:58 +0100

    Persist name data

--- Lobby/Lobby.gd
+++ Lobby/Lobby.gd
@@ -1,10 +1,17 @@
 extends Control
 
+onready var nameTextBox = $VBoxContainer/CenterContainer/GridContainer/NameTextBox
+
 func _ready() -> void:
-	$VBoxContainer/CenterContainer/GridContainer/NameTextBox.text = SaveGame.save_data['player_name']
+	nameTextBox.text = SaveGame.save_data['player_name']
 
 func _on_HostButton_pressed() -> void:
 	Network.create_server()
 
 func _on_JoinButton_pressed() -> void:
 	Network.connect_to_server()
+
+
+func _on_NameTextBox_text_changed(new_text: String) -> void:
+	SaveGame.save_data['player_name'] = nameTextBox.text
+	SaveGame.save_game()
