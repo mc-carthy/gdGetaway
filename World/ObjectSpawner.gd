e29bffce8b938aa2612325acde936d798703e3a7
Mick McCarthy <mick@mccarthy.tech>
2020-05-01 20:46:42 +0100

    Remove print statements

--- World/ObjectSpawner.gd
+++ World/ObjectSpawner.gd
@@ -17,16 +17,13 @@ func place_cars() -> void:
 	for i in range(number_of_parked_cars):
 		var tile = tile_list.pop_front()
 		var tile_type = gridmap.get_cell_item(tile.x, 0, tile.z)
-		print(tile_type)
 		var allowed_rotations = $ObjectRotationLookup.lookup_tile_rotation(tile_type)
-		print(allowed_rotations)
 		if allowed_rotations:
 			var tile_rotation: int = allowed_rotations[randi() % allowed_rotations.size() - 1] * -1
 			tile.y += 0.5
 			rpc('spawn_car', tile, tile_rotation)
 
 sync func spawn_car(tile: Vector3, rotation: int) -> void:
-	print('Spawning car')
 	var car = preload('res://Props/ParkedCars.tscn').instance()
 	car.translation = Vector3(tile.x * TILE_SIZE + TILE_SIZE / 2, tile.y, tile.z * TILE_SIZE + TILE_SIZE / 2)
 	car.rotation_degrees.y = rotation
