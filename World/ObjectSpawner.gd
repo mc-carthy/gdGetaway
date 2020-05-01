7c55a9ba44feca1bd81e9676f8bc22ffe2d77a39
Mick McCarthy <mick@mccarthy.tech>
2020-05-01 21:40:28 +0100

    Traffic cone spawner

--- World/ObjectSpawner.gd
+++ World/ObjectSpawner.gd
@@ -9,12 +9,14 @@ var map_size: Vector2 = Vector2.ZERO
 
 var number_of_parked_cars = 10
 var number_of_billboards = 10
+var number_of_traffic_cones = 10
 
 func generate_props(tile_list, size: Vector2) -> void:
 	tiles = tile_list
 	size = map_size
 	place_cars()
 	place_billboards()
+	place_traffic_cones()
 
 func place_cars() -> void:
 	var tile_list = random_tile(number_of_parked_cars)
@@ -32,6 +34,23 @@ sync func spawn_car(tile: Vector3, rotation: int) -> void:
 	car.translation = Vector3(tile.x * TILE_SIZE + TILE_SIZE / 2, tile.y, tile.z * TILE_SIZE + TILE_SIZE / 2)
 	car.rotation_degrees.y = rotation
 	add_child(car, true)
+
+func place_traffic_cones() -> void:
+	var tile_list = random_tile(number_of_traffic_cones)
+	for i in range(number_of_traffic_cones):
+		var tile = tile_list.pop_front()
+		var tile_type = gridmap.get_cell_item(tile.x, 0, tile.z)
+		var allowed_rotations = $ObjectRotationLookup.lookup_tile_rotation(tile_type)
+		if allowed_rotations:
+			var tile_rotation: int = allowed_rotations[randi() % allowed_rotations.size() - 1] * -1
+			tile.y += 0.5
+			rpc('spawn_traffic_cones', tile, tile_rotation)
+
+sync func spawn_traffic_cones(tile: Vector3, rotation: int) -> void:
+	var traffic_cones = preload('res://Props/TrafficCones/TrafficCones.tscn').instance()
+	traffic_cones.translation = Vector3(tile.x * TILE_SIZE + TILE_SIZE / 2, tile.y, tile.z * TILE_SIZE + TILE_SIZE / 2)
+	traffic_cones.rotation_degrees.y = rotation
+	add_child(traffic_cones, true)
 
 func place_billboards() -> void:
 	var tile_list = random_tile(number_of_billboards)
